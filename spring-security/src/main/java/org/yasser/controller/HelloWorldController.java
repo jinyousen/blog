0c04f8f47325a0128753922f03ca514311ac11aa
weini <weini@dtstack.com>
2019-11-05 18:13:25 +0800

    [ADD] add spring-security init project

---
+++ spring-security/src/main/java/org/yasser/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package org.yasser.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * SLOGAN: 做一个会写四种茴，笨且自律的人
+ *
+ * @Author Yasser
+ * @Date 2019-11-05
+ * @Description
+ */
+
+@RestController
+public class HelloWorldController {
+
+
+    @GetMapping("hello-world")
+    public String helloWorld() {
+
+        return "hello world!";
+    }
+
+
+}
